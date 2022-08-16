77ce857e392c144a3c29da3ba9e61a27da7a46a3
miki4920 <miki4920@gmail.com>
2022-08-16 23:14:14 +0100

    Continued work on neat ai

--- ai.py
+++ ai.py
@@ -8,28 +8,38 @@ xpos_end = 0
 
 
 def evaluate_genomes(genomes, config):
+    global environments, genomes_list, nets
+    environments = []
+    genomes_list = []
+    nets = []
     for genome_id, genome in genomes:
-        pass
+        environments.append(Environment())
+        genomes_list.append(genome)
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        nets.append(net)
+        genome.fitness = 0
 
-
-environment = Environment()
-while True:
     environment.create_ground()
     environment.render_environment()
     environment.get_keys()
     pygame.event.pump()
     frames_per_second.tick(60)
-    print(pygame.surfarray.array3d(game_display.copy()))
-    # print(image[:, :, ::-1])
-# config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
-#                      neat.DefaultSpeciesSet, neat.DefaultStagnation,
-#                      'config-feedforward')
-#
-# p = neat.Population(config)
-#
-# p.add_reporter(neat.StdOutReporter(True))
-# stats = neat.StatisticsReporter()
-# p.add_reporter(stats)
-# p.add_reporter(neat.Checkpointer(10))
-#
-# winner = p.run(evaluate_genomes)
+
+
+def run(config_path):
+    config = neat.config.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                                neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                                config_path)
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    winner = p.run(evaluate_genomes, 50)
+    print('\nBest genome:\n{!s}'.format(winner))
+
+
+if __name__ == "__main__":
+    config_path = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                         neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                         'config-feedforward')
+    run(config_path)
